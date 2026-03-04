75c29177b15bd8a540a1db08e460bdfa88910567
sehaj <singhsehajsukhleen@gmail.com>
2026-03-04 21:59:23 +0530

    updated the schema model

--- backend/schema_models/request.py
+++ backend/schema_models/request.py
@@ -9,3 +9,7 @@ class UsrRequest(BaseModel):
             description="the query of the user which will be feeded to llm ")
         ]
 
+    CONFIG : Annotated[
+        dict,
+        Field(...,description="the config parameter for our llm node ")
+        ] 

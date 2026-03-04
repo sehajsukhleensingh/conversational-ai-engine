11fc5bc174674d68c7f76490cafca928b37ea06e
sehaj <singhsehajsukhleen@gmail.com>
2026-03-04 21:59:04 +0530

    implemnted OOPS in config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,11 @@
-CONFIG = {"configurable":{"thread_id":"1"}}
+from backend.utils.helper import Utitlity
+
+
+class Config:
+
+    def config_id():
+
+        thread_id = Utitlity.generate_thread_id()
+        CONFIG = {"configurable":{"thread_id":thread_id}}
+
+        return CONFIG

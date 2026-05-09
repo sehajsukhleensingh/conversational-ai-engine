d98e4406cb45b241ea5f6c4b49249c9e857d95bd
sehaj <singhsehajsukhleen@gmail.com>
2026-05-09 15:52:30 +0530

    added the authorisation for github

--- backend/utils/mcp.py
+++ backend/utils/mcp.py
@@ -1,10 +1,21 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
+import os 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+git_api_key = os.getenv("GITHUB_TOKEN") 
+if not git_api_key:
+    raise ValueError("github api key not found")
 
 client = MultiServerMCPClient(
     {
         "github":{
             "transport" : "streamable_http" , 
-            "url" : "https://api.githubcopilot.com/mcp/"
+            "url" : "https://api.githubcopilot.com/mcp/" , 
+            "headers" :{
+                "Authorization":f"Bearer {os.getenv('GITHUB_TOKEN')}"
+            }
         }
     }
 )

af2c487b4083f8411396673f260b9f32edcd29c2
sehaj <singhsehajsukhleen@gmail.com>
2026-05-09 11:52:22 +0530

    updated the architecture and integrated the mcp

--- backend/core/chatbot.py
+++ backend/core/chatbot.py
@@ -8,6 +8,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import BaseMessage , HumanMessage , SystemMessage 
 
 from backend.utils.tools import search_tool , exchange_rate
+from backend.utils.mcp import client
 
 from dotenv import load_dotenv
 from typing import TypedDict , Annotated 
@@ -18,44 +19,44 @@ import asyncio
 load_dotenv()
 
 llm = ChatOpenAI(
-                model="gpt-4o-mini",  # or gemini, claude etc
-                temperature=0.2,
-                max_tokens=512,
-                base_url="https://api.aicredits.in/v1",
-            )
+            model="gpt-4o-mini",  # or gemini, claude etc
+            temperature=0.2,
+            max_tokens=512,
+            base_url="https://api.aicredits.in/v1",
+        )
 
 class BotState(TypedDict):
     # this will store all the chat logs , the convos of human and llm 
     messages : Annotated[list[BaseMessage],add_messages]
 
 
-tools = [exchange_rate , search_tool] #added tools to a list 
-
-llm_w_tools = llm.bind_tools(tools=tools)
-
-toolnode = ToolNode(tools=tools)
-
-
-async def chatfunc(state : BotState):
-    
-    # user query 
-    usr_query = state["messages"]
-    #feed to llm
-    system_message = SystemMessage(content="""
-    You are an assistant.
-    - NEVER show raw tool output to the user
-    - ALWAYS summarize tool results
-    - ONLY return final, clean answers
-    """) 
-    llm_res = await llm_w_tools.ainvoke([system_message] + usr_query)
-
-    return {"messages":[llm_res]}
-
 
 async def build_graph():
 
     conn = await aiosqlite.connect(database="backend/data/convos.db") 
     checkpointer = AsyncSqliteSaver(conn=conn)
+
+    mcp_tools = await client.get_tools()
+    tools = [mcp_tools , search_tool , exchange_rate]
+
+    llm_w_tools = llm.bind_tools(tools=tools)
+
+    async def chatfunc(state : BotState):
+    
+        # user query 
+        usr_query = state["messages"]
+        #feed to llm
+        system_message = SystemMessage(content="""
+        You are an assistant.
+        - NEVER show raw tool output to the user
+        - ALWAYS summarize tool results
+        - ONLY return final, clean answers
+        """) 
+        llm_res = await llm_w_tools.ainvoke([system_message]+usr_query)
+
+        return {"messages":[llm_res]}
+
+    toolnode = ToolNode(tools=tools)
 
     graph = StateGraph(BotState)
 
